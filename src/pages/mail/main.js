bc8d34c0a6789ac4ff24f09af9236cf1882f6966
Iván De los Santos García <ivandega22@gmail.com>
2019-04-06 12:02:34 +0200

    Added alert messages

--- src/pages/mail/main.js
+++ src/pages/mail/main.js
@@ -6,6 +6,13 @@ import axios from 'axios'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faBell, faComments} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faBell, faComments)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
