a1db3c4c49ad82a536d2d684df354039c9aab913
Agustín Núñez <seviagus@gmail.com>
2019-04-12 17:56:37 +0200

    Terms and condition i18n (Partially)

--- vue.config.js
+++ vue.config.js
@@ -11,8 +11,18 @@ module.exports = {
       // chunks to include on this page, by default includes
       // extracted common chunks and vendor chunks.
       chunks: ['chunk-vendors', 'chunk-common', 'index']
-    },
-    'about': {
+    }, 'terms_and_conditions': {
+      // entry for the page
+      entry: 'src/pages/terms_and_conditions/main.js',
+      // the source template
+      template: 'public/index.html',
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: 'Terms and conditions',
+      // chunks to include on this page, by default includes
+      // extracted common chunks and vendor chunks.
+      chunks: ['chunk-vendors', 'chunk-common', 'terms_and_conditions']
+    },'about': {
       // entry for the page
       entry: 'src/pages/about/main.js',
       // the source template
