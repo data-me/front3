6422458c7467d98811cda25e7dd3d9fecd32685d
Waco <wacomacaco@gmail.com>
2019-05-03 21:02:33 +0200

    Correcciones varias

--- src/pages/explore/App.vue
+++ src/pages/explore/App.vue
@@ -161,7 +161,7 @@
             type="text"
             v-model="form.title"
             id="title"
-            :state="form.title.length > 0"
+            :state="form.title.length >= 5"
             :maxlength="80"
             aria-describedby="titleHelpBlock"
           />
@@ -172,7 +172,7 @@
             type="text"
             id="description"
             v-model="form.description"
-            :state="form.description.length > 0"
+            :state="form.description.length >= 10"
             aria-describedby="descriptionHelpBlock"
           />
           <b-form-text id="descriptionHelpBlock">{{$t('description_offer_placeholder')}}</b-form-text>
